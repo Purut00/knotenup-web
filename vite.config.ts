5ee01491ce1586458be145f85525d002e8ccba98
purut00 <1nnylmh@gmail.com>
2025-12-23 09:59:02 +0800

    update vendor berat

--- vite.config.ts
+++ vite.config.ts
@@ -12,13 +12,20 @@ export default defineConfig({
         // 2. Teknik Manual Chunks (Paling Berkesan)
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // Asingkan Library Berat
+            
+            // 1. Library Paling Berat (Wajib Asingkan)
             if (id.includes('firebase')) return 'firebase';
-            if (id.includes('@google/generative-ai')) return 'gen-ai';
+            if (id.includes('jspdf') || id.includes('html2canvas')) return 'pdf-lib';
+            if (id.includes('fontawesome') || id.includes('@fortawesome')) return 'icons';
+            
+            // 2. Library Sederhana Berat
             if (id.includes('leaflet')) return 'leaflet';
             if (id.includes('swiper')) return 'swiper';
-            
-            // Bakinya masuk ke vendor
+            if (id.includes('@google/generative-ai') || id.includes('@google/genai')) return 'gen-ai';
+            if (id.includes('lucide')) return 'lucide';
+            if (id.includes('vue-datepicker') || id.includes('vue-easy-lightbox')) return 'ui-libs';
+
+            // 3. Bakinya barulah masuk vendor (sepatutnya dah ringan)
             return 'vendor';
           }
         }
