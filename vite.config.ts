72869838d26fbf7f87e2f052c535c8b56362af34
purut00 <1nnylmh@gmail.com>
2025-12-23 09:52:40 +0800

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // 🔥 TAMBAH INI UNTUK FIX ISU DATEPICKER 🔥
-optimizeDeps: {
-    exclude: ['@vuepic/vue-datepicker']  }
-})
+  build: {
+    // 1. Naikkan limit amaran supaya tak bising sangat
+    chunkSizeWarningLimit: 1500, 
+    
+    rollupOptions: {
+      output: {
+        // 2. Teknik Manual Chunks (Paling Berkesan)
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Asingkan Library Berat
+            if (id.includes('firebase')) return 'firebase';
+            if (id.includes('@google/generative-ai')) return 'gen-ai';
+            if (id.includes('leaflet')) return 'leaflet';
+            if (id.includes('swiper')) return 'swiper';
+            
+            // Bakinya masuk ke vendor
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    exclude: ['@vuepic/vue-datepicker']
+  }
+});
